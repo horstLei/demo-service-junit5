6dabc2eea7e805ebc4cc7d5d537a53bac10a135a
horst <horst.leitenmueller@it4health.at>
2025-06-17 09:26:53 +0200

    deleted the wrong content

--- demo-test-ejb/src/test/java/at/it4health/test/serviceb/DemoServiceBTest.java
+++ demo-test-ejb/src/test/java/at/it4health/test/serviceb/DemoServiceBTest.java
@@ -1,5 +1,6 @@
 package at.it4health.test.serviceb;
 
+import at.it4health.modulea.service.ModuleAService;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit5.container.annotation.ArquillianTest;
 import org.junit.jupiter.api.Tag;
@@ -12,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class DemoServiceBTest extends BaseDemoServiceBTest {
 
     @Test
-    @Tag("simpleTest")
+    @Tag("failOnFirst")
     @OperateOnDeployment("app_services")
     public void functionATest() {
 
@@ -20,4 +21,27 @@ public class DemoServiceBTest extends BaseDemoServiceBTest {
         fail("fail is not called");
     }
 
+    @Test
+    @Tag("failOnSecond")
+    @OperateOnDeployment("app_services")
+    public void functionBTest() {
+        System.out.println("functionBTest executed...");
+        fail("fail is not called");
+    }
+
+    @Test
+    @Tag("assertOnFirst")
+    @OperateOnDeployment("app_services")
+    public void functionCTest() {
+        System.out.println("functionCTest executed...");
+        assertEquals(1, 1);
+    }
+
+    @Test
+    @Tag("assertOnSecond")
+    @OperateOnDeployment("app_services")
+    public void functionDTest() {
+        System.out.println("functionDTest executed...");
+        assertEquals(1, -1);
+    }
 }
